3dad144a3e4e92c4bcfa42307ca6c1c43be66020
Daniel Ayala <dayala+github@protonmail.com>
2019-06-30 03:46:15 +0000

    Added ability to delete places.

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -27,6 +27,11 @@ class PlacesController < ApplicationController
     @place = Place.find(params[:id])
     @place.update_attributes(place_params)
     redirect_to root_path
+  
+  def destroy
+    @place = Place.find(params[:id])
+    @place.destroy
+    redirect_to root_path
   end
 
   private
